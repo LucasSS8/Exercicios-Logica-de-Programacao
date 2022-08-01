4e4d5cd6aeb39b72ca564682c22ee36892adeb44
Lucas Silva <lucas.souza_82@hotmail.com>
2022-08-01 20:46:24 -0300

    Exercicio 1

---
+++ Exercicio 1/Ex1Java.java
@@ -0,0 +1,17 @@
+public class Ex1Java{
+//Exercicio 1
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Valor de A:" + a);
+        System.out.println("Valor de B:" + b);
+        
+    }
+    
+}
---
+++ Exercicio 1/python.py
@@ -0,0 +1,10 @@
+#Exercicio 1
+
+a = 10;
+b = 20;
+temp = a;
+a = b
+b = temp;
+
+vars = "Valor de A = " + repr(a) + " e valor de B = " + repr(b);
+print(vars);
