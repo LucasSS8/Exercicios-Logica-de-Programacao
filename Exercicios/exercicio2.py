d73d33cbf9c185e94d07da725e260cbe9020509b
Lucas Silva <lucas.souza_82@hotmail.com>
2022-08-01 21:42:28 -0300

    Exercicio 2 python

---
+++ Exercicios/exercicio2.py
@@ -0,0 +1,23 @@
+#Exercicio 1
+
+#A
+print("A)")
+a = 10;
+b = 20;
+print(b);
+b = 5;
+print (a , b);
+
+#B
+print("B)");
+c = 30;
+d = 20;
+e = c + d;
+print(e);
+d = 10;
+print (d , e);
+e = c + d;
+print ( c , d , e);
+
+
+
