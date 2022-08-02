ffd6b01c967bd34d76a7ddd01fba464b0a447092
Lucas Silva <lucas.souza_82@hotmail.com>
2022-08-01 21:14:43 -0300

    Diretorio Exercicios

---
+++ Exercicios/Java1.java
@@ -0,0 +1,19 @@
+package Exercicios;
+
+public class Java1{
+//Exercicio 1
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Valor de A:" + a);
+        System.out.println("Valor de B:" + b);
+        
+    }
+    
+}
---
+++ Exercicios/Java2.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Java2 {
+
+    public static void main(String[] args){
+
+        //A)
+        int a = 10;
+        int b = 20;
+        System.out.println("b = " + b);
+        b = 5;
+        System.out.println("a = " + a + " b = "+ b);
+    
+    }
+   
+}
