3aedd2bcc98f1f05fbb078018e51b540439c61f6
Lucas Silva <lucas.souza_82@hotmail.com>
2022-08-04 22:39:49 -0300

    Exercicio 7 em python

---
+++ Exercicios/exercicio7.py
@@ -0,0 +1,9 @@
+#Exercicio 7
+anos = int(input("Digite quantos anos de idade você tem:"));
+meses = int(input("Digite quantos meses de idade você tem :"));
+dias = int(input("Digite quantos dias de idade você tem :"));
+diastotal = ((anos*365) + (meses*30) + dias);
+print("Você tem "+ repr(diastotal) +" dias de idade");
+
+
+
